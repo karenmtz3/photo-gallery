e45c18b63bc54d5af60d2df2283508e2f3ae184e
karenmtz3 <karenmtz273@gmail.com>
2020-08-10 17:59:32 -0500

    files modified

--- gallery/src/app/tab2/tab2.page.ts
+++ gallery/src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PhotoService } from './../services/photo.service';
+import { Photo } from './../models/photo.interface';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  public photos: Photo[] = [];
 
-  constructor() {}
+  constructor(private PhotoService: PhotoService) { }
 
+  ngOnInit(){
+    this.PhotoService.loadSaved().then(() => {
+      this.photos = this.PhotoService.getPhotos();
+    });
+  }
+
+  public newPhoto(): void{
+    this.PhotoService.addNewToGallery();
+  }
 }
